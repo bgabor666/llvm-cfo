8b02ac6021a4933d13a07bc16a340acb1b8d906c
Gabor Ballabas <gaborb@inf.u-szeged.hu>
2017-08-24 13:48:15 +0200

    Remove old, unused code and fix recursion issue

--- CFO.cpp
+++ CFO.cpp
@@ -18,188 +18,6 @@ using namespace llvm;
 STATISTIC(CFOCounter, "Counts number of functions greeted");
 
 namespace {
-  struct CFO : public FunctionPass {
-    static char ID; // Pass identification, replacement for typeid
-
-    typedef typename std::vector<const Instruction*> VectorType;
-
-    CFO() : FunctionPass(ID) {}
-
-    unsigned getHash(const Instruction& inst) {
-      unsigned hash = 0;
-      hash = inst.getOpcode() << 20;
-      hash |= inst.getNumOperands() << 16;
-      for (User::const_op_iterator oi = inst.op_begin(), on = inst.op_end(); oi != on; ++oi)
-        hash |= (hash & 0xffff0000) | ((hash & 0xffff) << 4) | (intptr_t(oi->get()) & 0xffff);
-      return hash;
-    }
-
-    bool runOnFunction(Function &F) override {
-      MapVector<unsigned, VectorType> map;
-      //SetVector<BasicBlock*> visited;
-      MapVector<unsigned, VectorType> siblings;
-      MapVector<unsigned, VectorType> families;
-
-      ++CFOCounter;
-      Function::BasicBlockListType& bbs = F.getBasicBlockList();
-      for (Function::const_iterator bbi = bbs.begin(), bbe = bbs.end(); bbi != bbe; ++bbi) {
-        errs() << "BB";
-        const BasicBlock& bb = *bbi;
-        //visited.insert
-
-        const_pred_iterator pi = pred_begin(&bb), pe = pred_end(&bb);
-        int pn = 0;
-        for (; pi != pe; ++pi)
-          ++pn;
-
-        succ_const_iterator si = succ_begin(&bb), se = succ_end(&bb);
-        int sn = 0;
-        for (; si != se; ++si)
-          ++sn;
-
-        errs() << " (pred: " << pn << ", succ: " << sn << ")\n";
-
-        const BasicBlock::InstListType& insts = bb.getInstList();
-        for (BasicBlock::const_iterator ii = insts.begin() , ie = insts.end(); ii != ie; ++ii) {
-          const Instruction& inst = *ii;
-          unsigned hash = getHash(inst);
-          errs() << "II: " << hash << " - " << inst << "\n";
-
-          map[hash].push_back(&inst);
-        }
-      }
-
-      errs() << "CFO: ";
-      errs().write_escaped(F.getName()) << '\n';
-
-      for (MapVector<unsigned, VectorType>::const_iterator bi = map.begin(), ei = map.end(); bi != ei; ++bi) {
-        if (bi->second.size() < 2)
-          continue;
-        errs() << "Hash: " << bi->first << "\n";
-        errs() << "Instructions: " << "\n";
-        const VectorType& v = bi->second;
-        errs() << "v.size(): " << v.size() << "\n";
-        for (int i = 0, n = v.size(); i < n; ++i) {
-          const Instruction* inst = v[i];
-          const BasicBlock* parent = inst->getParent();
-          inst->dump();
-        }
-      }
-      return false;
-    }
-  };
-}
-
-char CFO::ID = 0;
-static RegisterPass<CFO> X("cfo", "CFO World Pass");
-
-namespace {
-  struct CFO2 : public FunctionPass {
-    static char ID; // Pass identification, replacement for typeid
-    CFO2() : FunctionPass(ID) {}
-
-    bool runOnFunction(Function &F) override {
-      ++CFOCounter;
-      errs() << "CFO2: ";
-      errs().write_escaped(F.getName()) << '\n';
-      return false;
-    }
-
-    // We don't modify the program, so we preserve all analyses.
-    void getAnalysisUsage(AnalysisUsage &AU) const override {
-      AU.setPreservesAll();
-    }
-  };
-}
-
-char CFO2::ID = 0;
-static RegisterPass<CFO2>
-Y("cfo2", "CFO World Pass (with getAnalysisUsage implemented)");
-
-
-// Rewrite CFO pass
-namespace {
-  struct CFO3 : public FunctionPass {
-    static char ID; // Pass identification, replacement for typeid
-
-    typedef typename std::vector<const Instruction*> VectorType;
-
-    CFO3() : FunctionPass(ID) {}
-
-    unsigned getHash(const Instruction& inst) {
-      unsigned hash = 0;
-      hash = inst.getOpcode() << 20;
-      hash |= inst.getNumOperands() << 16;
-      for (User::const_op_iterator oi = inst.op_begin(), on = inst.op_end(); oi != on; ++oi)
-        hash |= (hash & 0xffff0000) | ((hash & 0xffff) << 4) | (intptr_t(oi->get()) & 0xffff);
-      return hash;
-    }
-
-    bool runOnFunction(Function &F) override {
-      MapVector<unsigned, VectorType> map;
-      //SetVector<BasicBlock*> visited;
-      MapVector<unsigned, VectorType> siblings;
-      MapVector<unsigned, VectorType> families;
-
-      ++CFOCounter;
-      Function::BasicBlockListType& bbs = F.getBasicBlockList();
-      for (Function::const_iterator bbi = bbs.begin(), bbe = bbs.end(); bbi != bbe; ++bbi) {
-        errs() << "BB";
-#if 0
-        const BasicBlock& bb = *bbi;
-        //visited.insert
-
-        const_pred_iterator pi = pred_begin(&bb), pe = pred_end(&bb);
-        int pn = 0;
-        for (; pi != pe; ++pi)
-          ++pn;
-
-        succ_const_iterator si = succ_begin(&bb), se = succ_end(&bb);
-        int sn = 0;
-        for (; si != se; ++si)
-          ++sn;
-
-        errs() << " (pred: " << pn << ", succ: " << sn << ")\n";
-#endif
-
-        const BasicBlock::InstListType& insts = bbi->getInstList();
-        for (BasicBlock::const_iterator ii = insts.begin() , ie = insts.end(); ii != ie; ++ii) {
-          const Instruction& inst = *ii;
-          unsigned hash = getHash(inst);
-          errs() << "II: " << hash << " - " << inst << "\n";
-
-          map[hash].push_back(&inst);
-        }
-      }
-
-      errs() << "CFO: ";
-      errs().write_escaped(F.getName()) << '\n';
-
-      for (MapVector<unsigned, VectorType>::const_iterator bi = map.begin(), ei = map.end(); bi != ei; ++bi) {
-        if (bi->second.size() < 2)
-          continue;
-        errs() << "Hash: " << bi->first << "\n";
-        errs() << "Instructions: " << "\n";
-        const VectorType& v = bi->second;
-        errs() << "v.size(): " << v.size() << "\n";
-        for (int i = 0, n = v.size(); i < n; ++i) {
-          const Instruction* inst = v[i];
-          const BasicBlock* parent = inst->getParent();
-          inst->dump();
-        }
-      }
-      return false;
-    }
-  };
-}
-
-char CFO3::ID = 0;
-//static RegisterPass<CFO3>
-//Z("cfo3", "CFO World Pass (with getAnalysisUsage implemented)");
-
-
-// Rewrite CFO pass to inherit from ModulePass
-namespace {
   static Function *getMyConstantFunction(Module &M, LLVMContext &Context) {
     Function *MyConstantF = cast<Function>(M.getOrInsertFunction("myConstant", Type::getInt32Ty(Context)));
 
@@ -215,11 +33,11 @@ namespace {
     return MyConstantF;
   }
 
-  struct CFO4 : public ModulePass {
+  struct CFO : public ModulePass {
     static char ID; // Pass identification, replacement for typeid
     SmallVector<Instruction*, 128> DeleteList;
 
-    CFO4() : ModulePass(ID) {}
+    CFO() : ModulePass(ID) {}
 
     bool runOnModule(Module &M) override {
 
@@ -265,7 +83,7 @@ namespace {
         errs() << "  BasicBlock<" << BB.getName() << ">" << ":\n";
 
         for (auto &I : BB) {
-          if (F.getName() == "parametricSub" && I.getOpcode() == Instruction::Sub) {
+          if (F.getName() != "parametricSub" && I.getOpcode() == Instruction::Sub) {
             errs() << "Found sub instruction...";
             DeleteList.push_back(&I);
             errs() << "getNumOperands(): " <<  I.getNumOperands() << "\n";
@@ -282,7 +100,7 @@ namespace {
   };
 }
 
-char CFO4::ID = 0;
-static RegisterPass<CFO4>
-Z("cfo4", "CFO World Pass (ModulePass)");
+char CFO::ID = 0;
+static RegisterPass<CFO>
+Z("cfo", "CFO World Pass (ModulePass)");
 
